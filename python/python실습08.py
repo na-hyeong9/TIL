358cb30bcf321f28216033dcd01a6b36c741eb23
na_hyeong <skgud9511@gmail.com>
2022-07-13 12:14:52 +0900

    python 함수_ 220713

--- python/python실습08.py
+++ python/python실습08.py
@@ -3,11 +3,7 @@ numbers = [0, 20, 100]
 first = numbers[0]
 second = numbers[0]
 for i in numbers:
-    if first <= i:
-     m = i
-     elif:
-        remove(first)
-     elif m <= i:
-     second = i
+   if first <= i:
+      second = first
+      first = n
 print(second)
-    
